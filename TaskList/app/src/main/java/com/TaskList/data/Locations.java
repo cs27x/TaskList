8daa4b0426ede5f84fd04491f737502c7db6dcf5
WillPascucci <william.m.pascucci@vanderbilt.edu>
2015-02-01 15:07:48 -0600

    data models

---
+++ TaskList/app/src/main/java/com/TaskList/data/Locations.java
@@ -0,0 +1,27 @@
+package com.TaskList.data;
+
+/**
+ * Created by willpascucci on 2/1/15.
+ */
+
+import java.util.ArrayList;
+
+public class Locations {
+
+    private ArrayList<String> locations;
+
+    public Locations() {
+    }
+    
+    public Locations(ArrayList<String> locations) {
+        this.locations = locations;
+    }
+
+    public ArrayList<String> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(ArrayList<String> locations) {
+        this.locations = locations;
+    }
+}

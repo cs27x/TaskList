93a63a765a9c711ed493198baa84881840a3b607
sahilpanjwani <sahilpanjwani12@gmail.com>
2015-02-01 22:31:47 -0600

    Update and rename calc_total_utility.java to get_importance.java

--- TaskList/app/src/main/java/com/TaskList/api/calc_total_utility.java
+++
@@ -1,5 +0,0 @@
-package com.TaskList.api;
-
-/* Calculate total utility for a task based on importance rating */
-
-/* Also known as Ui */
---
+++ TaskList/app/src/main/java/com/TaskList/api/get_importance.java
@@ -0,0 +1,4 @@
+package com.TaskList.api;
+
+/* Get importance */
+

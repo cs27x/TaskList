780712616a12d9796a053569e1e05a370b5ca9a9
sahilpanjwani <sahilpanjwani12@gmail.com>
2015-02-01 22:49:24 -0600

    Update and rename calc_time_required.java to calc_time_required_task.java

--- TaskList/app/src/main/java/com/TaskList/api/calc_time_required.java
+++
@@ -1,7 +0,0 @@
-package com.TaskList.api;
-
-/* calculate time required */
-
-/* For tasks- Initially calculated based on time required ratings, then updated based on % completed */
-
-/* For events- time slot */
---
+++ TaskList/app/src/main/java/com/TaskList/api/calc_time_required_task.java
@@ -0,0 +1,6 @@
+package com.TaskList.api;
+
+/* calculate time required for a task*/
+
+/* Initially calculated based on time required ratings, then updated based on % completed */
+

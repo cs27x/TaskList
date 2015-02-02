d91b1993c8f929e1a9a31a6a6335ab95cf882f20
sahilpanjwani <sahilpanjwani12@gmail.com>
2015-02-01 22:35:27 -0600

    Update calc_time_required.java

--- TaskList/app/src/main/java/com/TaskList/api/calc_time_required.java
+++ TaskList/app/src/main/java/com/TaskList/api/calc_time_required.java
@@ -2,4 +2,6 @@ package com.TaskList.api;
 
 /* calculate time required */
 
-/* Initially calculated based on time required ratings, then updated based on % completed */
+/* For tasks- Initially calculated based on time required ratings, then updated based on % completed */
+
+/* For events- time slot */
